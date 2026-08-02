ad1909969d8f9bbd54315eaf0be10a3e223a31a1
dushyant24bcon2017-collab <dushyant.24bcon2017@jecrcu.edu.in>
2026-08-02 15:47:43 +0530

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.py
@@ -0,0 +1,23 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        indices = dict()
+        #Storing all the elements with their indexes
+        #when we are doing 2 pass approach we also need to take care 
+        #that we are not including the same index twice so we apply
+        #cond where we check both of their index are not same 
+        # indices[diff]!=i
+        for i , n in enumerate(nums):
+            indices[n]=i
+
+        for i , n in enumerate(nums):
+            diff = target - n 
+            if(diff in  indices and indices[diff]!=i):
+                return [i,indices[diff]] 
+        
+        return []
+            
+
+        
+        
+
+        
